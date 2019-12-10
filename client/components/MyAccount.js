bea0192eac9c5ef1eff00c2f782698e368517435
Lyndsey Reese <lmariereese@gmail.com>
2019-12-10 17:43:21 -0500

    MyAccount component: Change email section styles

--- client/components/MyAccount.js
+++ client/components/MyAccount.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class MyAccount extends React.Component {
   constructor(props) {
@@ -6,16 +7,27 @@ class MyAccount extends React.Component {
     this.state = {
       currentPassword: '',
       newPassword: '',
-      confirmNew: ''
+      confirmNew: '',
+      toggleEdit: false,
+      email: ''
     };
     this.handleChange = this.handleChange.bind(this);
+    this.toggleEdit = this.toggleEdit.bind(this);
+  }
+
+  toggleEdit() {
+    this.setState(state => ({
+      toggleEdit: !state.toggleEdit
+    }));
   }
 
   handleChange(event) {
     event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
+    console.log(this.state.currentPassword);
     return (
       <div className="main-content-wrapper">
         <div>
@@ -27,34 +39,79 @@ class MyAccount extends React.Component {
             <div className="profile-section">
               <h4>Email Address</h4>
               <div>
-                <span>{}</span>
-                <button type="button">Edit</button>
+                {this.state.toggleEdit ? (
+                  <div className="email-update-form">
+                    <input
+                      placeholder="enter a new email address"
+                      type="email"
+                      name="email"
+                      id="email-update-input"
+                      value={this.state.email}
+                      onChange={event => this.handleChange(event)}
+                    />
+                    <div id="email-update-btn-div">
+                      <button type="button" className="inline">
+                        Update
+                      </button>
+                      <button
+                        type="button"
+                        className="inline"
+                        onClick={this.toggleEdit}
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                ) : (
+                  <div className="email-update-form">
+                    <span className="display-email">
+                      {this.props.userEmail}
+                    </span>
+                    <button
+                      type="button"
+                      className="inline"
+                      onClick={this.toggleEdit}
+                    >
+                      Edit
+                    </button>
+                  </div>
+                )}
               </div>
             </div>
             <div className="profile-section">
               <h4>Password</h4>
               <div className="password-update-form">
-                <label htmlFor="current-password">Current Password</label>
+                <label htmlFor="currentPassword">Current Password</label>
                 <input
                   placeholder="enter your current password"
                   type="password"
-                  name="current-password"
+                  name="currentPassword"
+                  className="account-input"
+                  onChange={event => this.handleChange(event)}
+                  value={this.state.currentPassword}
                 />
-                <hr />
-                <label htmlFor="new-password">New Password</label>
+                <label htmlFor="newPassword">New Password</label>
                 <input
                   placeholder="enter a new password"
                   type="password"
-                  name="new-password"
+                  name="newPassword"
+                  className="account-input"
+                  onChange={event => this.handleChange(event)}
+                  value={this.state.newPassword}
                 />
                 <label>Confirm New Password</label>
                 <input
                   htmlFor="confirm-new-password"
                   placeholder="enter the password again"
                   type="password"
-                  name="confirm-new-password"
+                  name="confirmNew"
+                  className="account-input"
+                  onChange={event => this.handleChange(event)}
+                  value={this.state.confirmNew}
                 />
-                <button type="button">Update Password</button>
+                <button type="button" className="update-password-btn">
+                  Update Password
+                </button>
               </div>
             </div>
           </div>
@@ -64,4 +121,12 @@ class MyAccount extends React.Component {
   }
 }
 
-export default MyAccount;
+const mapState = state => ({
+  userEmail: state.user.email
+});
+
+// const mapDispatch = dispatch => ({
+
+// })
+
+export default connect(mapState)(MyAccount);

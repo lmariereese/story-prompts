e983f86bec44e12f4ee0a5b8651c5fe31f3e6cd9
Lyndsey Reese <lmariereese@gmail.com>
2019-12-12 11:19:38 -0500

    ShareButton component skeleton

---
+++ client/components/ShareButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class ShareButton extends React.Component {
+  render() {
+    return (
+      <div>
+        <button type="button">Share</button>
+      </div>
+    );
+  }
+}

ae31bbbf067806510f9ff9071e5a47faae367077
Lyndsey Reese <lmariereese@gmail.com>
2020-01-24 15:07:09 -0500

    TodaysPrompt component: created file and skeleton

---
+++ client/components/TodaysPrompt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TodaysPrompt = props => {
+  return (
+    <div>
+      <div>
+        <h2>Today's Prompt</h2>
+        <h4>date placeholder</h4>
+      </div>
+      <div />
+    </div>
+  );
+};
+
+export default TodaysPrompt;

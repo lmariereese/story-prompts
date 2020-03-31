fd47487265ad73477f7a6fa514639758df234137
Lyndsey Reese <lmariereese@gmail.com>
2020-03-31 14:49:55 -0400

    ShareableLinkPopup shows full url

--- client/components/ShareableLinkPopup.js
+++ client/components/ShareableLinkPopup.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import {sharePrompt} from '../store/reducers/storyElements';
 // import copyLinkIcon from '../../public/copyLinkIcon.png';
+const url = 'localhost:8080';
 
 class ShareableLinkPopup extends React.Component {
   componentDidMount() {
@@ -25,13 +26,10 @@ class ShareableLinkPopup extends React.Component {
   }
 
   render() {
-    console.log('current in popup', this.props.current);
-    console.log('location in popup', this.props.location);
     return (
       <div>
-        <p>this is the popup!</p>
         {this.props.current.urlToken ? (
-          <p>{`${this.props.location.pathname}/${
+          <p>{`${url}${this.props.location.pathname}/${
             this.props.current.urlToken
           }`}</p>
         ) : (
